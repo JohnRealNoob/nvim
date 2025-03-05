a190c1d746154cc17932b1cfeb9156edea46a6cc
JohnRealNoob <nutt.john12@gmail.com>
2025-03-04 21:34:21 -0700

    remap

--- lua/johnrealnoob/remap.lua
+++ lua/johnrealnoob/remap.lua
@@ -1,6 +1,4 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>t", ":terminal<CR>")
 
 vim.keymap.set("v", "J", ":m ' >+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m ' >+2<CR>gv=gv")
@@ -9,5 +7,7 @@ vim.keymap.set("v", "Y", "yg$")
 
 vim.keymap.set("n", "<leader>h", "<C-w>h")
 vim.keymap.set("n", "<leader>l", "<C-w>l")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
 
 vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
